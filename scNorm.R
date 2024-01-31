2e91639f2e2fed8c4e87f77e4ed7d16b930ee0d3
Kei <keiwebber3@gmail.com>
2024-01-31 10:03:21 -0700

    final

--- scNorm.R
+++ scNorm.R
@@ -1,24 +1,51 @@
 library(SCnorm)
 library(tidyverse)
+library(dplyr)
+library(limma)
 
 
-filepaths = c("20min_January/report.pg_matrix.tsv")
-MS2_filenames = c("20min_January/report-first-pass.pg_matrix.tsv")
+filepaths = c("20min_January/report-first-pass.pg_matrix.tsv",
+ "20min_January/report.pg_matrix.tsv",
+"10min_January/report-first-pass.pg_matrix.tsv",
+"10min_January/report.pg_matrix.tsv",
+
+"20min_January/report-first-pass.pr_matrix.tsv",
+"20min_January/report.pr_matrix.tsv",
+"10min_January/report-first-pass.pr_matrix.tsv",
+"10min_January/report.pr_matrix.tsv")
+
 group_names = list()
 group_names[[filepaths[1]]] = c("Colony 1.3", "Colony 1.6")
+group_names[[filepaths[2]]] = c("Colony 1.3", "Colony 1.6")
+group_names[[filepaths[3]]] = c("Colony 1.3", "Colony 1.6")
+group_names[[filepaths[4]]] = c("Colony 1.3", "Colony 1.6")
+group_names[[filepaths[5]]] = c("Colony 1.3", "Colony 1.6")
+group_names[[filepaths[6]]] = c("Colony 1.3", "Colony 1.6")
+group_names[[filepaths[7]]] = c("Colony 1.3", "Colony 1.6")
+group_names[[filepaths[8]]] = c("Colony 1.3", "Colony 1.6")
 
+#isProtein = TRUE
+isProteins = c(TRUE,TRUE,TRUE,TRUE,FALSE,FALSE,FALSE,FALSE)
 
 
 group_filters = list()
 group_filters[[filepaths[1]]] = c("Col1-3","Col1-6")
-out_filter = c("M@di")
+group_filters[[filepaths[2]]] = c("Col1-3","Col1-6")
+group_filters[[filepaths[3]]] = c("Col1-3","Col1-6")
+group_filters[[filepaths[4]]] = c("Col1-3","Col1-6")
+group_filters[[filepaths[5]]] = c("Col1-3","Col1-6")
+group_filters[[filepaths[6]]] = c("Col1-3","Col1-6")
+group_filters[[filepaths[7]]] = c("Col1-3","Col1-6")
+group_filters[[filepaths[8]]] = c("Col1-3","Col1-6")
+
+out_filter = c("N3","N4","H6","M3","L3","J10","J11")
 
 ################Functions################
 
 DIANN.AbundanceMatrix = function(proteinFile, isProtein=TRUE, eachFilenameConvention = ".d") {
   if (isProtein){
     y = read_tsv(proteinFile) %>%
-      filter(!grepl("contam", Protein.Group)) %>%
+      filter(!grepl("contam", Protein.Group),!grepl("Keratin",First.Protein.Description)) %>%
       dplyr::select(contains(eachFilenameConvention), contains( "Protein.Ids")) %>%
       mutate(Accession = Protein.Ids) %>%
       select(-Protein.Ids)
@@ -32,8 +59,12 @@ DIANN.AbundanceMatrix = function(proteinFile, isProtein=TRUE, eachFilenameConven
   y
 }
 
-DIANN.FilterByName = function(abundances, filterIn, filterOut){
-  name = "Accession"
+DIANN.FilterByName = function(abundances, filterIn, filterOut, isProtein = TRUE){
+  if(isProtein){
+    name = "Accession"
+  } else {
+    name = "Annotated Sequence"
+  }
   for(eachFilter in filterIn) {
     abundances = dplyr::select_if(abundances, 
                                              grepl(paste0(name,"|",eachFilter),
@@ -48,65 +79,96 @@ DIANN.FilterByName = function(abundances, filterIn, filterOut){
 }
 ################Import##############
 
-y = list()
-yms2 = list()
+
 j = 1
 for (filepath in filepaths){
-  x = DIANN.AbundanceMatrix(proteinFile = filepath)
-  x2 = DIANN.AbundanceMatrix(proteinFile = MS2_filenames[j])
+  y = list()
+  isProtein = isProteins[j]
+  x = DIANN.AbundanceMatrix(proteinFile = filepath, isProtein = isProtein)
+  OUTPUT_FILENAME = paste0(gsub("\\.tsv","",filepath),"_normalized.tsv")
   
-  i = 1
+  i = 1  
   for(eachGroup in group_names[[filepath]]) {
     y[[eachGroup]] = x %>%
-      DIANN.FilterByName(filterIn = group_filters[[filepath]][i], filterOut = out_filter)
-    
-    yms2[[eachGroup]] = x2 %>%
-      DIANN.FilterByName(filterIn = group_filters[[filepath]][i], filterOut = out_filter)
+      DIANN.FilterByName(filterIn = group_filters[[filepath]][i], filterOut = out_filter, isProtein = isProtein)
     i = i + 1
+    
   }
-  j = j + 1
-}
+  
 
 
+  remove(x)
 
 ################Make Matrix################
-
-Conditions = c()
-i = 1
-
-for (filepath in filepaths){
+  if(isProtein){
+    name = "Accession"
+    new_name = "Protein.Ids"
+  } else {
+    name = "Annotated Sequence"
+    new_name = "Stripped.Sequence"
+  }
+  
+  Conditions = c()
+  maxnumcells = 0
+  i = 1
+  
+ 
   for(eachGroup in group_names[[filepath]]) {
     mycols = colnames(y[[eachGroup]])
-    mycols = mycols[mycols != "Accession"]
-    myrows = y[[eachGroup]][["Accession"]]
+    mycols = mycols[mycols != name]
+    myrows = y[[eachGroup]][[name]]
     current = y[[eachGroup]][mycols] %>%
       as.matrix()
     colnames(current) = mycols
     rownames(current) = myrows
+    current = current[rowSums(is.na(current)) != ncol(current),]
     if(i==1){
       DIANN_report_matrix = current
     } else {
       DIANN_report_matrix = current %>%
-        merge(DIANN_report_matrix)
+        merge(DIANN_report_matrix,by="row.names")
     }
-    Conditions = c(Conditions,rep(eachGroup,length(mycols)))
+    numcells = length(mycols)
+      maxnumcells = numcells
+    Conditions = c(Conditions,rep(eachGroup,numcells))
     i = i + 1
     
   }
-}
-
-DIANN_report_matrix = DIANN_report_matrix %>%
-  t()
-    
+  
+  
+  newcols = DIANN_report_matrix[,1]
+  DIANN_report_matrix = DIANN_report_matrix[,-1]
+  rownames(DIANN_report_matrix) = newcols
+  DIANN_report_matrix[is.na(DIANN_report_matrix)] = 0
+  
+  remove(current)
+  remove(y)
+ 
 ################Normalize################
 
 
-mySCData = SingleCellExperiment::SingleCellExperiment(assays = list('counts' = DIANN_report_matrix))
-
-
-exampleGC <- runif(dim(mySCData)[1], 0, 1)
-names(exampleGC) <- rownames(mySCData)
-
-DataNorm <- SCnorm(mySCData, Conditions,
-                   PrintProgressPlots = TRUE,
-                     FilterCellNum = 10, withinSample = exampleGC)
+  mySCData = SingleCellExperiment::SingleCellExperiment(assays = list('counts' = DIANN_report_matrix))
+  
+  
+  exampleGC = runif(dim(mySCData)[1], 0, 1)
+  names(exampleGC) = rownames(mySCData)
+  minCells = floor(maxnumcells*0.7) #30% missing values and above will be removed
+  
+  DataNorm = SCnorm(mySCData, Conditions,
+                     PrintProgressPlots = TRUE,
+                       FilterCellNum = minCells, withinSample = exampleGC,
+                     NCores=1)
+  
+  NormedData = SingleCellExperiment::normcounts(DataNorm)
+  
+  NormedData[NormedData == 0] = NA
+  
+  proteinIDs = row.names(NormedData)
+  
+  NormedData =as.data.frame(NormedData)
+  
+  NormedData[new_name] = proteinIDs
+  
+  write_tsv(NormedData, OUTPUT_FILENAME)
+  j = j + 1
+}
